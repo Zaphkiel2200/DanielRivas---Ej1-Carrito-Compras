6c2e6fa27b03bd3821cb113240ce67ebeff13a72
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2026-02-23 19:34:53 -0500

    Feat: Usestate

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const products = [
     { id: 1, nombre: 'Laptop', precio: 1200 },
     { id: 2, nombre: 'Smartphone', precio: 800 },
@@ -5,10 +7,12 @@ const products = [
 ];
 
 const ShoppingCart = () => {
+    const [cart, setCart] = useState(products);
+
     return (
         <div className="shopping-cart-container">
             <div className="cart-badge">
-                Cantidad de productos: {products.length}
+                Cantidad de productos: {cart.length}
             </div>
         </div>
     );
